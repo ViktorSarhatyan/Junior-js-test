2a770a01a80aa29f3724779fe60945f617c9d429
ViktorSarhatyan <viktorsarhatyan@gmail.com>
2022-03-24 17:47:00 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+    let sum=0;
+  for(let i=0;i<cartItemsPrices.length;i++){
+    sum += +cartItemsPrices[i];
+  }
+  return +sum.toFixed(2) === WINNING_SUM;
 }
 
 module.exports = doesCartWinPrize;
